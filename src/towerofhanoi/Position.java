7342fe41bbbcc6212668643879619dbe60b86ea6
Lamiek Haile <hailelamiek41@gmail.com>
2026-09-08 14:41:50 -0400

    Add tower position enum

---
+++ src/towerofhanoi/Position.java
@@ -0,0 +1,33 @@
+// Project 3 Summer II 2026
+// Virginia Tech Honor Code Pledge:
+
+//
+
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+
+// -- Your name (lamiekh)
+
+// LLM Statement:
+
+// I have not used any assistance for the assignment beyond course resources and
+// staff.
+
+package towerofhanoi;
+
+/**
+ * Represents the three possible positions of a Tower in the puzzle, plus a
+ * DEFAULT value used only for testing the default case of a switch statement.
+ *
+ * @author Lamiek Haile
+ * @version 8/3/2026
+ */
+public enum Position
+{
+    LEFT,
+    MIDDLE,
+    RIGHT,
+    DEFAULT;
+}
